d1fc840d7f90a2c1f27ae5bc68d5868d058304af
hzliwenhao <hzliwenhao@corp.netease.com>
2017-08-26 11:35:57 +0800

    完成客户中心的开发

--- templets/dongnan/js/custom.js
+++ templets/dongnan/js/custom.js
@@ -1,10 +1,13 @@
 /**
  * Created by hzliwenhao on 2017/8/19.
  */
-function addClickEventForRTab() {
-  $('div.nr li,div.nr h4').click(function () {
-    console.log(this);
-    var SID = $(this).attr('sid')
-    $.cookie("SID", SID);
+
+// 改变新闻中心list_news4中的图片的显示位置.
+function changePicLocation() {
+  $('.news_con>dl>dt').each(function (index, element) {
+    if ((index + 1) % 2 == 0) {
+      $(element).removeClass("fr");
+      $(element).addClass("fl");
+    }
   });
 }
